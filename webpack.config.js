4cdaa8416970534d7c57aaea4d4c13a6b94c4a38
savaukr <sava_ukr@ukr.net>
2021-05-21 16:09:44 +0300

    simple component is ssr

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = (env) => {
     const modules = {
@@ -25,11 +26,32 @@ module.exports = (env) => {
                 }
             ],
         },
+        stylusIsomorph: {
+            test: /\.styl$/,
+            use: [
+                {
+                    loader: MiniCssExtractPlugin.loader,
+                },
+                {
+                    loader: "css-loader",
+                },
+                {
+                    loader: "stylus-loader",
+                    options: {
+                        import: [
+                            path.resolve(__dirname, './src/Common/Styles/variables.styl'),
+                        ],
+                    }
+                },
+            ],
+        },
     }
 
     if (env === 'production') {
         modules.stylus.use.splice(2, 0, { loader: "postcss-loader" })
+        modules.stylusIsomorph.use.splice(2, 0, { loader: "postcss-loader" })
     }
+
 
     const resolve = {
         extensions: [".ts", ".tsx", ".js", ".jsx"],
