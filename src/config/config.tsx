32f2e565be481987adde942beaf5704fd5192738
savaukr <sava_ukr@ukr.net>
2021-06-15 12:40:21 +0300

    before add Ceil

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -1,3 +1,3 @@
 export const M = 100; // кількість стрічок
 export const N = 100; //кількість стовпчиків
-export const X = 3; // кількість близьких Amount
+export const X = 7; // кількість близьких Amount
